3a31f228b0579af2f7c0bba73f905813ab4aad10
amirreza <amirrezakh123@gmail.com>
2025-05-06 15:36:35 +0330

    ➕ Add: Register/Login Buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="p-10">
+    <main className="flex flex-col items-center justify-center w-full h-screen gap-4">
       <h1 className="text-2xl font-bold">Digital Products Marketplace</h1>
-      <Button>Get Started</Button>
+      <div className="flex items-center gap-4">
+        <Link href={'/register'}><Button>Register</Button></Link>
+        <Link href={'/login'}><Button>Login</Button></Link>
+      </div>
     </main>
   );
 }
