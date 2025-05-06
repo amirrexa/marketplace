2c7ceb99008e9e807a327e9b2ded6edf79ec134b
amirreza <amirrezakh123@gmail.com>
2025-05-06 21:33:31 +0330

    🔧 Fix: Additional Request Param

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -2,7 +2,7 @@ import { verifyJwtEdge } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PATCH(req: NextRequest) {
     const token = req.cookies.get("token")?.value;
     const payload = await verifyJwtEdge(token || "");
 
@@ -10,14 +10,17 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
         return Response.json({ message: "Forbidden" }, { status: 403 });
     }
 
+    const url = new URL(req.url);
+    const id = url.pathname.split("/").pop();
+
     const { role: newRole } = await req.json();
 
-    if (!newRole || !["BUYER", "SELLER", "ADMIN"].includes(newRole)) {
-        return Response.json({ message: "Invalid role" }, { status: 400 });
+    if (!id || !newRole || !["BUYER", "SELLER", "ADMIN"].includes(newRole)) {
+        return Response.json({ message: "Invalid data" }, { status: 400 });
     }
 
     await prisma.user.update({
-        where: { id: params.id },
+        where: { id },
         data: { role: newRole },
     });
 
